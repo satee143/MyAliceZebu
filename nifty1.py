d0e9d65850f07f8b29579f89217af14539c0892e
satheesh <satee143@gmail.com>
2020-08-28 11:00:59 +0530

    updated pwd

--- nifty1.py
+++ nifty1.py
@@ -1,12 +1,13 @@
 import csv
 import datetime
 from time import sleep
-
+import logging
+logging.basicConfig(level=logging.DEBUG)
 from alice_blue import *
 
-access_token = AliceBlue.login_and_get_access_token(username='AB102865', password='sampath@9', twoFA='a',
+access_token = AliceBlue.login_and_get_access_token(username='AB102865', password='sampath@1', twoFA='a',
                                                     api_secret='CW7LT01PAQRAVVUFQ0VH0PGXV1VJUG10RGWK3IAMIJGHJ1KCXYU1QBZWQJZ1FR53')
-alice = AliceBlue(username='AB102865', password='sampath@9', access_token=access_token,
+alice = AliceBlue(username='AB102865', password='sampath@1', access_token=access_token,
                   master_contracts_to_download=['NFO'])
 socket_opened = True
 
@@ -27,6 +28,6 @@ alice.start_websocket(subscribe_callback=event_handler_quote_update,
                       run_in_background=True)
 while (socket_opened == False):
     pass
-alice.subscribe(alice.get_instrument_for_fno(symbol='NIFTY', expiry_date=datetime.date(2020, 8, 27), is_fut=True,
+alice.subscribe(alice.get_instrument_for_fno(symbol='NIFTY', expiry_date=datetime.date(2020, 9,24), is_fut=True,
                                              strike=None, is_CE=False), LiveFeedType.MARKET_DATA)
 sleep(99999)
